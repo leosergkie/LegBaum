1cbef564b2687fcea9532eaab6b318b73d459612
Dmitry <32599856+barsukov-dmitry@users.noreply.github.com>
2018-09-23 17:49:29 +0300

    Исправил клики. и position: fixed;

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,13 @@
 $('.menu-icon').click(function(){
     $(".menu-hidden").animate({left: '0'}, 400);
-	$(".menu-close img").fadeIn(400);
+	$(".menu-close").fadeIn(400);
 	$(".menu-icon").fadeOut(400);
+	$(".menu-bg").fadeIn(400);
 });
 
-$('.menu-close, .nav').click(function(){
+$('.menu-bg, .nav, .menu-close').click(function(){
     $(".menu-hidden").animate({left: '-320px'},400);
-	$(".menu-close img").fadeOut(400);
+	$(".menu-close").fadeOut(400);
 	$(".menu-icon").fadeIn(400);
+	$(".menu-bg").fadeOut(400);
 });
