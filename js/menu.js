0f512f5857196a64b2be3fa6f068f5538cd43b33
Dmitry <32599856+barsukov-dmitry@users.noreply.github.com>
2018-09-23 17:20:57 +0300

    Починил кнопку закрытия

--- js/menu.js
+++ js/menu.js
@@ -1,31 +1,11 @@
-/*после первого клика по бургеру 
-второй клик (в любом месте) закрывает меню*/
-let every_second_click = 0;
-var delay = 400;
-//$(".menu-close").fadeOut(1, 0);
 $('.menu-icon').click(function(){
-    var lastTime = +localStorage.lastTime;
-    var nowTime = +new Date();
-    if (lastTime && (lastTime + delay > nowTime)) {
-            setTimeout(function f(){}, 400);
-            return false;
-        } else {
-           localStorage.lastTime = nowTime;
-        }
-     $(".menu-hidden").animate({left: '0'}, 400);
-	 //$(".menu-close").fadeIn(300, 1);
-    if (every_second_click != 2) {
-        every_second_click = 1;
-    }
+    $(".menu-hidden").animate({left: '0'}, 400);
+	$(".menu-close img").fadeIn(400);
+	$(".menu-icon").fadeOut(400);
 });
 
-$(document).click(function(){
-    if (every_second_click == 2) {
-        $(".menu-hidden").animate({left: '-350px'},400);
-        every_second_click = 0;
-		$(".menu-close").fadeOut(300, 0);
-    }
-    if (every_second_click == 1) {
-        every_second_click++;
-    }
+$('.menu-close, .nav').click(function(){
+    $(".menu-hidden").animate({left: '-320px'},400);
+	$(".menu-close img").fadeOut(400);
+	$(".menu-icon").fadeIn(400);
 });
