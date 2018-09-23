a08cd0c72bad305f8b50135adce7336589609665
Dmitry <32599856+barsukov-dmitry@users.noreply.github.com>
2018-09-23 15:00:09 +0300

    Исправил шрифт, меню выезжает слева, крестик закрытия не работает как надо. Надо править.

--- js/menu.js
+++ js/menu.js
@@ -1,28 +1,31 @@
 /*после первого клика по бургеру 
 второй клик (в любом месте) закрывает меню*/
 let every_second_click = 0;
-var delay = 500;
+var delay = 400;
+//$(".menu-close").fadeOut(1, 0);
 $('.menu-icon').click(function(){
-	var lastTime = +localStorage.lastTime;
-	var nowTime = +new Date();
-	if (lastTime && (lastTime + delay > nowTime)) {
-            setTimeout(function f(){}, 500);
+    var lastTime = +localStorage.lastTime;
+    var nowTime = +new Date();
+    if (lastTime && (lastTime + delay > nowTime)) {
+            setTimeout(function f(){}, 400);
             return false;
         } else {
            localStorage.lastTime = nowTime;
         }
-  	$(".menu-hidden").fadeIn("slow");
-  	if (every_second_click != 2) {
-  		every_second_click = 1;
-  	}
+     $(".menu-hidden").animate({left: '0'}, 400);
+	 //$(".menu-close").fadeIn(300, 1);
+    if (every_second_click != 2) {
+        every_second_click = 1;
+    }
 });
 
 $(document).click(function(){
-	if (every_second_click == 2) {
-		$(".menu-hidden").fadeOut("slow");
-		every_second_click = 0;
-	}
-	if (every_second_click == 1) {
-		every_second_click++;
-	}
+    if (every_second_click == 2) {
+        $(".menu-hidden").animate({left: '-350px'},400);
+        every_second_click = 0;
+		$(".menu-close").fadeOut(300, 0);
+    }
+    if (every_second_click == 1) {
+        every_second_click++;
+    }
 });
