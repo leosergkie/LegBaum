80ab68134f65579bb28c338bbce5141bcedd560c
Vlad <39775639+Nakmak98@users.noreply.github.com>
2018-09-24 17:17:58 +0300

    Добавление новых участников комады

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,9 @@
+x = 0;
+var second_name = $('.member-second-name').attr('name');
+var first_name = $('.member-first-name').attr('name');
+
+
+
 $('.burger').click(function(){
     $(".menu-hidden").animate({left: '0'}, 400);
 	$(".menu-close").fadeIn(400);
@@ -11,4 +17,31 @@ $('.grid, .nav, .menu-close').click(function(){
 	$(".menu-icon").fadeIn(400);
 	$(".menu-bg").fadeOut(400);
 });
+
+
+ {
+	$('#add').click(function(){
+	x++;
+	console.log(x);
+	var second_name_id = 'second-name';
+	var first_name_id = 'first-name';
+	$("<div class='member-inputs'><div class='member-inputs-title'>Участник</div><label class='form-label' for='memberSecondName'>Фамилия</label><input class='member-second-name form' id='second-name' name='memberSecondName' type='text'><label class='form-label' for='memberFirstName'>Имя</label><input class='member-first-name form' id='first-name' name='memberFirstName' type='text'></div>").insertBefore($(".add-member"));
+	$('#'+ second_name_id).attr('id', second_name_id+x);
+	$('#'+ first_name_id).attr('id', first_name_id+x);
+	$('#'+ second_name_id+x).attr('name', second_name+x);
+	$('#'+ first_name_id+x).attr('name', first_name+x);
+	$('.team-inputs').css("grid-template-rows", function(i,val){
+	var arr = val.split(' ');	
+	arr.splice(-2, 0, '169.375px');
+	var str = arr.join(' ');
+	return str;
+    });
+
+	if(x == 7) { $('.add-member').remove() }
+});
+}
+
+
+
+
 
