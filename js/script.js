cf9d442a1e5d55910d66f3ca9f74ccd0bf48f23a
Galina <helllina99@gmail.com>
2018-09-27 00:08:43 +0300

    Готовые страницы команд. Юзерская и админская версии, соответственно в папках team_page и team_page_admin.

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//var jqxhr = $.getJSON("test.json")
+var json = { "router" : [
+				a:{				
+					"team_name" : "team_name",
+					"fail_count" : 0,
+					"start_time" : 0,
+					"task_list" : [],
+					"team_id" : 6,
+					"players_count" : 4
+				}
+]
+}
+alert(json.a.team_name);
