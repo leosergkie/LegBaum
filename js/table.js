87566df1296e0277b09d910a706e24f88eef6e2e
Koolana <kolya_andreychik@mail.ru>
2018-09-29 15:19:50 +0300

    Reverse sort

--- js/table.js
+++ js/table.js
@@ -6,9 +6,17 @@ var arrAlgSort = ['team_id', 'name', 'leader_name',
 
 var currSort = '';
 
+var sortReverse = 1;
+
 $(document).ready(function(){
     for(var i = 1; i<10;i++){
         $('.table_teams_head').children('.table_teams_stroke').children('#' + i).mousedown(function(eventObject){
+            if(currSort != arrAlgSort[$(this).attr('id')-1]){
+                sortReverse = 1;
+            }else{
+                sortReverse*=-1;
+            }
+
             sortDATA(arrAlgSort[$(this).attr('id')-1]);
             /*console.log($(this).attr('id'));*/
             outputDATA();
@@ -120,48 +128,48 @@ function sortDATA(param){
 
 /*Условия сортировок*/
 function algSort_team_id(a, b){
-    if (a.team_id > b.team_id) return 1;
-    if (a.team_id < b.team_id) return -1;
+    if (a.team_id > b.team_id) return 1*sortReverse;
+    if (a.team_id < b.team_id) return -1*sortReverse;
 }
 
 function algSort_name(a, b){
-    if (a.name > b.name) return 1;
-    if (a.name < b.name) return -1;
+    if (a.name > b.name) return 1*sortReverse;
+    if (a.name < b.name) return -1*sortReverse;
 }
 
 function algSort_leader_name(a, b){
-    if (a.leader_name > b.leader_name) return 1;
-    if (a.leader_name < b.leader_name) return -1;
+    if (a.leader_name > b.leader_name) return 1*sortReverse;
+    if (a.leader_name < b.leader_name) return -1*sortReverse;
 }
 
 function algSort_number_of_tasks(a, b){
-    if (a.number_of_tasks > b.number_of_tasks) return 1;
-    if (a.number_of_tasks < b.number_of_tasks) return -1;
+    if (a.number_of_tasks > b.number_of_tasks) return 1*sortReverse;
+    if (a.number_of_tasks < b.number_of_tasks) return -1*sortReverse;
 }
 
 function algSort_start_time(a, b){
-    if (UNIXTimeToNormalTime(a.start_time) > UNIXTimeToNormalTime(b.start_time)) return 1;
-    if (UNIXTimeToNormalTime(a.start_time) < UNIXTimeToNormalTime(b.start_time)) return -1;
+    if (UNIXTimeToNormalTime(a.start_time) > UNIXTimeToNormalTime(b.start_time)) return 1*sortReverse;
+    if (UNIXTimeToNormalTime(a.start_time) < UNIXTimeToNormalTime(b.start_time)) return -1*sortReverse;
 }
 
 function algSort_finish_time(a, b){
-    if (UNIXTimeToNormalTime(a.finish_time) > UNIXTimeToNormalTime(b.finish_time)) return 1;
-    if (UNIXTimeToNormalTime(a.finish_time) < UNIXTimeToNormalTime(b.finish_time)) return -1;
+    if (UNIXTimeToNormalTime(a.finish_time) > UNIXTimeToNormalTime(b.finish_time)) return 1*sortReverse;
+    if (UNIXTimeToNormalTime(a.finish_time) < UNIXTimeToNormalTime(b.finish_time)) return -1*sortReverse;
 }
 
 function algSort_fails_count(a, b){
-    if (a.fails_count > b.fails_count) return 1;
-    if (a.fails_count < b.fails_count) return -1;
+    if (a.fails_count > b.fails_count) return 1*sortReverse;
+    if (a.fails_count < b.fails_count) return -1*sortReverse;
 }
 
 function algSort_score(a, b){
-    if (a.score > b.score) return 1;
-    if (a.score < b.score) return -1;
+    if (a.score > b.score) return 1*sortReverse;
+    if (a.score < b.score) return -1*sortReverse;
 }
 
 function algSort_is_active(a, b){
-    if (a.is_active > b.is_active) return -1;
-    if (a.is_active < b.is_active) return 1;
+    if (a.is_active > b.is_active) return 1*sortReverse;
+    if (a.is_active < b.is_active) return -1*sortReverse;
 }
 
 function UNIXTimeToNormalTime(a){
